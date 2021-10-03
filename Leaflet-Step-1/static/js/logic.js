f324382e142a79d7e7cd6436e2384b40bd963416
Sharon Colson <sharon.colson@gmail.com>
2021-10-03 18:16:23 -0500

    changing data pull from all week to all month

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,6 +1,6 @@
 // Store our API endpoint as queryUrl.
 var queryUrl =
-  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
 // Perform a GET request to the query URL/
 d3.json(queryUrl).then(function (data) {
